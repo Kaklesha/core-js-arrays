14982f98a849127ea1b10234c26811b4d569723b
Kirill Kornilov <k-korni1ov@yandex.ru>
2024-01-13 00:17:38 +0400

    fear: shiftArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -601,8 +601,11 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const rightBranch = arr.slice(0, n);
+  const midBranch = arr.slice(n, n + 1);
+  const leftBranch = arr.slice(-n);
+  return [...leftBranch, ...rightBranch, ...midBranch].slice(0, arr.length);
 }
 
 /**
