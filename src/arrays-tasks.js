3dcf2d64179db3d532743c56e0ed707d92a783d0
Kirill Kornilov <k-korni1ov@yandex.ru>
2024-01-12 22:49:57 +0400

    feat: getMaxItems

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -514,8 +514,10 @@ function getHexRGBValues(/* arr */) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  const dump = arr.sort((a, b) => a - b).reverse();
+
+  return dump.slice(0, n);
 }
 
 /**
